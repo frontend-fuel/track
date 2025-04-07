83760148182922c64e160f8c71935d7c2ecd5bee
frontend-fuel <neverfail2026@gmail.com>
2025-04-07 21:26:54 +0530

    Update buzzer.js

--- buzzer.js
+++ buzzer.js
@@ -14,11 +14,11 @@ async function updateBuzzer(state) {
 
         if (parseInt(data) > 0) {
             if (state === 1) {
-                statusElement.textContent = "✅ Buzzer turned ON. It will auto OFF after 30 seconds.";
+                statusElement.textContent = "✅ Buzzer turned ON. It will auto OFF after 15 seconds.";
                 // Auto turn off after 30 seconds
                 setTimeout(() => {
                     updateBuzzer(0);
-                }, 30000);
+                }, 15000);
             } else {
                 statusElement.textContent = "🛑 Buzzer turned OFF.";
             }
